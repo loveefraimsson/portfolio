500137b90c2e07a0c42a7cc62a0df6d8a5e7569a
Love Efraimsson <loveefraimsson@gmail.com>
2024-03-14 15:53:02 +0100

    Added quiz to project, and styling for projects

--- src/project.js
+++ src/project.js
@@ -9,13 +9,22 @@ const projects = [
         linkGithub: 'https://github.com/loveefraimsson/tenzies_frontend'
     },
     {
+        title: 'Quiztime',
+        description: 'Quiztime är ett frågespel där användaren kan välja kategori, svårighetsgrad och antal frågor. Sedan kan man klicka på en knapp för att se hur många rätt man fick!',
+        imageURL: new URL('assets/quiz.png', import.meta.url).href,
+        imageAlt: 'Skärmklipp från Quiz-spelet',
+        tags: ['React', 'CSS', 'Vite', 'Node', 'API', 'Kryptera text'],
+        linkWebsite: 'https://quiztime-eight.vercel.app/',
+        linkGithub: 'https://github.com/loveefraimsson/tenzies_frontend'
+    },
+    {
         title: 'WorkoutBase',
         description: 'Workoutbase är ett projekt som jag gjorde som examensarbete på min utbildning. Man kan logga in och söka bland olika träningsövningar, göra sina egna träningsprogram och favoritmarkera övningar som man vill kolla tillbaka på vid ett senare tillfälle. Det finns även en webshop där man kan lägga sina valda varor i kundkorgen. Inloggning till projektet finns på Github.',
         imageURL: new URL('assets/workoutbase.png', import.meta.url).href,
         imageAlt: 'Skärmklipp från WorkoutBase-hemsidan',
         tags: ['React', 'Sass', 'MongoDB', 'Express.js', 'Node'],
         linkWebsite: 'https://workoutbase-frontend.vercel.app/',
-        linkGithub: 'https://github.com/loveefraimsson/workoutbase_frontend'
+        linkGithub: 'https://github.com/loveefraimsson/quiz/'
     }
 ]
 
